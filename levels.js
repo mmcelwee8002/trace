eb2cb14c9308df6ca49beb910baa3e7dc0c4e5f9
Matthew McElwee <mmcelwee8002@gmail.com>
2026-08-19 09:11:59 -0500

    grid size level 1

--- levels.js
+++ levels.js
@@ -7,14 +7,13 @@
 
 const levels = [
     {
-        id: "1-01",
-        title: "Getting Started",
-        size: 5,
-        start: [0, 0],
-        goal: [4, 4],
-        walls: [],
-        optimalLength: 9
-    },
+    id: "1-01",
+    title: "Getting Started",
+    size: 9,
+    start: [0, 0],
+    goal: [8, 8],
+    walls: []
+},
 
     {
         id: "1-02",
@@ -25,6 +24,54 @@ const levels = [
         walls: [
             [2, 2]
         ],
-        optimalLength: 9
-    }
+        
+    },
+{
+    id: "1-03",
+    title: "Around the Wall",
+    size: 5,
+    start: [0, 0],
+    goal: [4, 4],
+    walls: [
+        [0, 2],
+        [1, 2],
+        [2, 2],
+        [3, 2],
+        [3, 3]
+    ]
+},
+{
+    id: "1-04",
+    title: "The Detour",
+    size: 5,
+    start: [4, 0],
+    goal: [0, 4],
+    walls: [
+        [1, 1],
+        [1, 2],
+        [2, 2],
+        [3, 2],
+        [3, 3]
+    ]
+},
+
+{
+    id: "1-05",
+    title: "Think Ahead",
+    size: 5,
+    start: [0, 0],
+    goal: [4, 4],
+ walls: [
+    [0, 2],
+    [1, 2],
+    [2, 0],
+    [2, 2],
+    [2, 4],
+    [3, 4]
+]
+}
+
+    
+
+
 ];
